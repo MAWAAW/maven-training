4b7d06a8375da3afb116d3ddcbcbba5afe2fa804
MAWAAW <dark1111@hotmail.fr>
2017-11-23 15:29:53 +0100

    ajout profils

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,6 @@
 package dev;
 
+import java.util.ResourceBundle;
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -9,8 +10,14 @@ import com.github.lalyos.jfiglet.FigletFont;
 public class App 
 {
     public static void main( String[] args )
-    {
-    	String asciiArt = FigletFont.convertOneLine("Hello world !");
+    {  
+        String titre = ResourceBundle.getBundle("application").getString("titre");
+        String asciiArt = FigletFont.convertOneLine(titre);
         System.out.println(asciiArt);
+        
+        // affichage de l'environnement
+        String environnement = ResourceBundle.getBundle("application").getString(
+        "environnement");
+        System.out.println("Environnement : " + environnement);
     }
 }
